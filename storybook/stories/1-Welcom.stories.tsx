c8fa33f92b64e8b7579f7ec7842e6410e94d997d
Daybrush <daybrush@gmail.com>
2020-01-23 00:28:46 +0900

    fix: fix Welcome story

--- storybook/stories/1-Welcom.stories.tsx
+++ storybook/stories/1-Welcom.stories.tsx
@@ -70,65 +70,53 @@ story.add("Welcome", () => {
 
             <Selecto
                 dragContainer={window}
-                selectableTargets={["#selecto1 .cube", "#selecto2 .element", "#selecto3 li"]}
+                selectableTargets={["#selecto2 .element", "#selecto3 li"]}
                 onSelect={e => {
                     e.added.forEach(el => {
                         el.classList.add("selected");
                     });
                     e.removed.forEach(el => {
                         el.classList.remove("selected");
-                    })
+                    });
                 }}
                 hitRate={10}
                 selectFromInside={false}
                 selectByClick={false}
                 toggleContinueSelect={"shift"}
             ></Selecto>
-            <div className="elements" id="selecto1">
-                <h2>Select anything</h2>
-                <div className="selecto-area">
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                    <div className="cube"></div>
-                </div>
-            </div>
             <div className="elements selecto-area" id="selecto2">
-                <h2>Related Projects</h2>
+                <h2>📦 Select Packages.</h2>
                 <a className="element react" href="https://github.com/daybrush/selecto/tree/master/packages/react-selecto" target="_blank"><div className="symbol">R</div><div className="name">React</div></a>
                 <div className="element angular"><div className="symbol">Ng</div><div className="name">Angular</div></div>
                 <div className="element preact"><div className="symbol">Pr</div><div className="name">Preact</div></div>
                 <div className="element vue"><div className="symbol">V</div><div className="name">Vue</div></div>
                 <div className="element svelte"><div className="symbol">Sv</div><div className="name">Svelte</div></div>
                 <div className="element lit"><div className="symbol">L</div><div className="name">Lit</div></div>
+            </div>
+            <div className="elements selecto-area" id="selecto2">
+                <h2>🔥 Select Projects.</h2>
                 <div className="element scenejs"><div className="symbol">Sn</div><div className="name">Scene.js</div></div>
                 <div className="element moveable"><div className="symbol">Mv</div><div className="name">Moveable</div></div>
                 <div className="element drag"><div className="symbol">Dr</div><div className="name">Drag</div></div>
+                <div className="element ruler"><div className="symbol">Ru</div><div className="name">Ruler</div></div>
+                <div className="element drag"><div className="symbol">K</div><div className="name">Keycon</div></div>
                 <div className="element guides"><div className="symbol">Gd</div><div className="name">Guides</div></div>
             </div>
-            <div className="examples selecto-area" id="selecto3">
-                <h2>Examples</h2>
+            <div className="elements selecto-area" id="selecto3">
+                <h2>📝 Select Codes</h2>
                 <ol>
-                    <li>1. <a href="#" target="_parent">Select in real time.</a></li>
-                    <li>2. <a href="#" target="_parent">Only select at start and end.</a></li>
-                    <li>3. <a href="#" target="_parent">Only select at end.</a></li>
-                    <li>4. <a href="#" target="_parent">Continue to select.</a></li>
-                    <li>5. <a href="#" target="_parent">Continue to select through the toggle key.</a></li>
+                    <li>Press the <strong>A</strong> key or the <strong>Addons</strong> button to view the <strong>Code Preview</strong> tab.</li>
+                    <li>You can see the story on the screen as code in various frameworks, and in <strong>CodeSandBox</strong>.</li>
+                </ol>
+            </div>
+            <div className="elements selecto-area" id="selecto3">
+                <h2>🚀 Select Examples</h2>
+                <ol>
+                    <li><a href="#" target="_parent">Select in real time.</a></li>
+                    <li><a href="#" target="_parent">Only select at start and end.</a></li>
+                    <li><a href="#" target="_parent">Only select at end.</a></li>
+                    <li><a href="#" target="_parent">Continue to select.</a></li>
+                    <li><a href="#" target="_parent">Continue to select through the toggle key.</a></li>
                 </ol>
             </div>
             <div className="empty elements"></div>
