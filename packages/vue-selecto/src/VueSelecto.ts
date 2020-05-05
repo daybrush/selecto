a3c1de562bf43550c93b49317fabec17a2d3a364
daybrush <daybrush@gmail.com>
2020-05-06 00:45:53 +0900

    fix: fix vue watch

--- packages/vue-selecto/src/VueSelecto.ts
+++ packages/vue-selecto/src/VueSelecto.ts
@@ -1,14 +1,26 @@
-import VanillaSelecto, { SelectoOptions, OPTIONS, EVENTS, PROPERTIES, CLASS_NAME } from 'selecto';
+import VanillaSelecto, {
+    SelectoOptions, OPTIONS, EVENTS, PROPERTIES,
+    CLASS_NAME, METHODS, SelectoProperties, SelectoMethods
+} from 'selecto';
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import { CreateElement } from 'vue';
-import { Properties } from 'framework-utils';
+import { Properties, withMethods, MethodInterface } from 'framework-utils';
+import { isUndefined, IObject } from '@daybrush/utils';
 
 
-@Component({})
+const watches: IObject<any> = {};
+
+PROPERTIES.forEach(name => {
+    watches[name] = function (this: VueSelecto, val: any) {
+        this.updateProperty(name, val);
+    };
+})
+@Component({ watch: watches })
 @Properties(OPTIONS as any, (prototype, name) => {
     Prop()(prototype, name);
 })
 export default class VueSelecto extends Vue {
+    @withMethods(METHODS as any)
     private selecto!: VanillaSelecto;
     public render(h: CreateElement) {
         return h('div', {
@@ -17,10 +29,14 @@ export default class VueSelecto extends Vue {
         });
     }
     public mounted() {
+        const props = this.$props;
         const options: Partial<SelectoOptions> = {};
 
         OPTIONS.forEach((name) => {
-            options[name] = this.$props[name];
+            const value = props[name];
+            if (!isUndefined(value)) {
+                options[name] = value;
+            }
         });
 
         this.selecto = new VanillaSelecto({
@@ -36,17 +52,16 @@ export default class VueSelecto extends Vue {
             });
         });
     }
-    public updated() {
-        const props = this.$props;
+    public updateProperty(name: string, value: any) {
         const selecto = this.selecto;
 
-        PROPERTIES.forEach((name) => {
-            if (selecto[name] !== props[name]) {
-                (selecto as any)[name] = props[name];
-            }
-        });
+        (selecto as any)[name] = value;
     }
     public beforeDestroy() {
         this.selecto.destroy();
     }
 }
+
+export default interface VueSelecto extends SelectoProperties,
+    MethodInterface<SelectoMethods, VanillaSelecto, VueSelecto> { }
+
