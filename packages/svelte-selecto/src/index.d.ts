7ffa626fdba5e36f7c8345dcad4afbfcd936af91
daybrush <daybrush@gmail.com>
2020-09-18 04:09:52 +0900

    fix: fix svelte-moveable types

--- packages/svelte-selecto/src/index.d.ts
+++ packages/svelte-selecto/src/index.d.ts
@@ -1,28 +1,12 @@
-import VanillaSelecto, { SelectoMethods } from "selecto";
+import VanillaSelecto, { SelectoMethods, SelectoOptions } from "selecto";
+import { SvelteComponentDev } from "svelte/internal";
 import { MethodInterface } from "framework-utils";
 
-
-interface ComponentOptions {
-    target: HTMLElement;
-    anchor?: HTMLElement | null;
-    props?: {};
-    hydrate?: boolean;
-    intro?: boolean;
+export default class SelectoComponent<T={}> extends SvelteComponentDev {
+    $$prop_def: SelectoOptions & T;
+    getInstance(): VanillaSelecto;
+}
+export default interface SelectoComponent extends MethodInterface<SelectoMethods, VanillaSelecto, SelectoComponent> {
 }
 
-interface SelectoComponent extends MethodInterface<SelectoMethods, VanillaSelecto, SelectoComponent> {
-    new(options: ComponentOptions): any;
-    // client-side methods
-    $set(props: {}): void;
-    $on(event: string, callback: (event: CustomEvent) => void): void;
-    $destroy(): void;
-    // server-side methods
-    render(props?: {}): {
-        html: string;
-        css: { code: string; map: string | null };
-        head?: string;
-    };
-}
-
-export default SelectoComponent;
 export * from "selecto";
