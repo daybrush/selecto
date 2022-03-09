2e56ccc2b4727a75e9ddf1a53a5da342f8e5050c
Daybrush <daybrush@gmail.com>
2022-03-09 23:42:14 +0900

    fix: fix svelte types #71

--- packages/svelte-selecto/src/index.d.ts
+++ packages/svelte-selecto/src/index.d.ts
@@ -3,7 +3,7 @@ import { SvelteComponentDev } from "svelte/internal";
 import { MethodInterface } from "framework-utils";
 
 export default class SelectoComponent<T={}> extends SvelteComponentDev {
-    $$prop_def: SelectoOptions & T;
+    $$prop_def: Partial<SelectoOptions> & T;
     getInstance(): VanillaSelecto;
 }
 export default interface SelectoComponent extends MethodInterface<SelectoMethods, VanillaSelecto, SelectoComponent> {
