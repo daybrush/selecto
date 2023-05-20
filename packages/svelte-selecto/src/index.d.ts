58f2e898a03af4cfcb9038c6afc6951c5531f99b
Daybrush <younkue.choi@navercorp.com>
2023-05-20 16:43:51 +0900

    feat: support svelte types

--- packages/svelte-selecto/src/index.d.ts
+++ packages/svelte-selecto/src/index.d.ts
@@ -1,12 +1,16 @@
-import VanillaSelecto, { SelectoMethods, SelectoOptions } from "selecto";
-import { SvelteComponentDev } from "svelte/internal";
-import { MethodInterface } from "framework-utils";
+/// <reference types="svelte" />
+import { SvelteComponentTyped } from "svelte";
+import { SelectoMethods, SelectoOptions, SelectoEvents } from "selecto";
 
-export default class SelectoComponent<T={}> extends SvelteComponentDev {
-    $$prop_def: Partial<SelectoOptions> & T;
-    getInstance(): VanillaSelecto;
+export type SvelteSelectoEvents = {
+    [key in keyof SelectoEvents]: CustomEvent<SelectoEvents[key]>;
 }
-export default interface SelectoComponent extends MethodInterface<SelectoMethods, VanillaSelecto, SelectoComponent> {
+export default class SelectoComponent extends SvelteComponentTyped<
+    SelectoOptions,
+    SvelteSelectoEvents
+> { }
+
+export default interface SelectoComponent extends SelectoMethods {
 }
 
 export * from "selecto";
