491bf1019196c1ebabad1d7c8419384faa4f1c5c
Artur Androsovych <arthurandrosovich@gmail.com>
2023-06-06 09:21:25 +0300

    fix(ngx-selecto): reduce change detection cycles and teardown event listeners once the view is removed (#134)

--- packages/ngx-selecto/projects/ngx-selecto/src/lib/ngx-selecto.component.ts
+++ packages/ngx-selecto/projects/ngx-selecto/src/lib/ngx-selecto.component.ts
@@ -1,10 +1,24 @@
 import {
-  Component, ElementRef,
-  AfterViewInit, OnDestroy, OnChanges, SimpleChanges, ViewChild, Input, Output, EventEmitter,
+  Component,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  EventEmitter,
+  NgZone,
 } from '@angular/core';
+import { Subject, fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import VanillaSelecto, {
-  CLASS_NAME, OPTIONS, SelectoOptions, PROPERTIES, EVENTS,
+  CLASS_NAME,
+  OPTIONS,
+  SelectoOptions,
+  PROPERTIES,
+  EVENTS,
 } from 'selecto';
+
 import { ANGULAR_SELECTO_INPUTS, ANGULAR_SELECTO_OUTPUTS } from './consts';
 import { NgxSelectoInterface } from './ngx-selecto.interface';
 
@@ -15,22 +29,23 @@ import { NgxSelectoInterface } from './ngx-selecto.interface';
   `,
   inputs: ANGULAR_SELECTO_INPUTS,
   outputs: ANGULAR_SELECTO_OUTPUTS,
-  styles: []
 })
 export class NgxSelectoComponent
   extends NgxSelectoInterface
   implements OnDestroy, AfterViewInit, OnChanges {
-  @ViewChild('el', { static: false }) elRef!: ElementRef;
+  @ViewChild('el', { static: true }) elRef!: ElementRef<HTMLElement>;
 
   public selectionClassName = CLASS_NAME;
 
-  constructor() {
+  private destroy$ = new Subject<void>();
+
+  constructor(private ngZone: NgZone) {
     super();
+
     EVENTS.forEach(name => {
       (this as any)[name] = new EventEmitter();
     });
   }
-
 
   ngAfterViewInit(): void {
     const options: Partial<SelectoOptions> = {};
@@ -40,19 +55,24 @@ export class NgxSelectoComponent
         (options as any)[name] = this[name];
       }
     });
-    this.selecto = new VanillaSelecto({
+
+    this.selecto = this.ngZone.runOutsideAngular(() => new VanillaSelecto({
       ...options,
       portalContainer: this.elRef.nativeElement,
-    });
+    }));
 
     const selecto = this.selecto;
 
     EVENTS.forEach(name => {
-      selecto.on(name, e => {
-        this[name].emit(e as any);
+      fromEvent(selecto, name).pipe(takeUntil(this.destroy$)).subscribe(event => {
+        const emitter = this[name];
+        if (emitter && (emitter.observed || emitter.observers.length > 0)) {
+          this.ngZone.run(() => emitter.emit(event as any));
+        }
       });
     });
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     const selecto = this.selecto;
 
@@ -71,7 +91,9 @@ export class NgxSelectoComponent
       (selecto as any)[name] = currentValue;
     }
   }
+
   ngOnDestroy() {
+    this.destroy$.next();
     this.selecto.destroy();
   }
 }
