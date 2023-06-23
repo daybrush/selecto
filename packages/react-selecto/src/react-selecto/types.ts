00cfabaaf75693c1f27cf899b5626f8ff63856fe
Daybrush <younkue.choi@navercorp.com>
2023-06-23 10:13:18 +0900

    fix: fix react event types

--- packages/react-selecto/src/react-selecto/types.ts
+++ packages/react-selecto/src/react-selecto/types.ts
@@ -1,17 +1,6 @@
 import { SelectoEvents, SelectoOptions } from "selecto";
 
-export interface ReactSelectoEventNames {
-    onSelectStart: "selectStart";
-    onSelect: "select";
-    onSelectEnd: "selectEnd";
-    onDragStart: "dragStart";
-    onDrag: "drag";
-    onDragEnd: "dragEnd";
-    onKeydown: "keydown";
-    onKeyup: "keyup";
-    onScroll: "scroll";
-}
 export type SelectoEventProps = {
-    [key in keyof ReactSelectoEventNames]: (e: SelectoEvents[ReactSelectoEventNames[key]]) => any;
+    [key in keyof SelectoEvents as `on${Capitalize<key>}`]: (e: SelectoEvents[key]) => any;
 };
 export type SelectoProps = SelectoOptions & SelectoEventProps;
